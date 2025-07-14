93e6e9e8ec17682edf90ef076392bd7d99752cbe
Erick Domínguez <50849788+00437110@users.noreply.github.com>
2025-07-14 17:03:44 -0600

    Uploading to deploy 1
    Needed to change address. Uploading for testing

---
+++ src/components/Hero.jsx
@@ -0,0 +1,38 @@
+export default function Hero() {
+    return (
+        <>
+            <h5>Complete this training program to get SHREDDED</h5>
+            <ol className="benefits=list">
+                <li>Follow a simple program with great results: </li>
+                <li>Get fit, healthy, and shredded</li>
+                <li>Learn more of the gym, come for the trainning</li>
+                <li>Become a life time gym browth, it is for change ❤</li>
+
+            </ol>
+            <h3>The Rules</h3>
+            <p>To complete this program, you <b>MUST follow these 3 rules: </b></p>
+            <ul className="rules-list">
+                <div className="rule-item">
+                    <p><b>Rest</b></p>
+                    <p>Ensure you are taking rest days when necessary</p>
+                </div>
+                <div className="rule-item">
+                    <p><b>Reps</b></p>
+                    <p>Every rep is a pause rep following a 
+                        <abbr title="2 seconds down - 2 seconds pause - 2 seconds up"> 2-2-2 tempo</abbr> </p>
+                </div>
+                <div className="rule-item">
+                    <p><b>Weight*</b></p>
+                    <p>Select the maximum weight that allows you to complete the set with good form </p>
+                </div>
+            </ul>
+            <small>* The first and second set should be at 75% and 85% of
+                your working weight used by your last 2 sets</small>
+            <h3>The training plan: </h3>
+            <p>This traninig plan uses a structure known as <b>Bro Split </b>, and follows this rotation ⭐</p>
+            <p><b><i>Push &rarr; Pull &rarr; Legs &rarr; Repeat</i></b></p>
+            <p>Complete all the workouts below and track your progress along the way!</p>
+
+        </>
+    )
+}
